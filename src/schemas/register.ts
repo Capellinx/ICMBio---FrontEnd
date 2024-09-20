f8264818ee43ad18f6816a5e184c2311ebbd7193
Capellinx <capellaaa7@gmail.com>
2024-09-19 23:34:32 -0300

    feat: improve validation of password and add new key in schema (type_person)

--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -8,6 +8,7 @@ export const registerSchema = z.object({
       .string({ required_error: "Email obrigatório" })
       .min(1, { message: "Email obrigatório" })
       .email({ message: "Email inválido" }),
+   type_person: z.enum(["Pesquisador(a)", "Condutor(a)", "Voluntario(a)"], { required_error: "Tipo obrigatório" }),
    phone: z.coerce
       .string({ required_error: "Telefone obrigatório" })
       .min(1, { message: "Telefone obrigatório" }),
@@ -21,7 +22,11 @@ export const registerSchema = z.object({
       }, "CPF deve conter apenas números"),
    password: z
       .string({ required_error: "Senha obrigatória" })
-      .min(1, { message: "Senha obrigatória" }),
+      .min(1, { message: "Senha obrigatória" })
+      .regex(/[0-9]+/, "É necessário pelo menos um numero")
+      .regex(/(?=.*?[a-z])/, "É necessário pelo menos uma letra minúscula")
+      .regex(/(?=.*?[A-Z])/, "É necessário pelo menos uma letra maiúscula")
+      .regex(/[^A-Za-z0-9]/, "É necessário pelo menos um caracter especial"),
    confirmPassword: z
       .string({ required_error: "Confirmar Senha obrigatória" })
       .min(1, { message: "Confirmar Senha obrigatória" })
