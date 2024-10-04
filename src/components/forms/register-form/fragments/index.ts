837f4e333a78420ed19f5cba520016fc834e712e
Capellinx <capellaaa7@gmail.com>
2024-10-03 22:59:32 -0300

    feat: agroup imports

---
+++ src/components/forms/register-form/fragments/index.ts
@@ -0,0 +1,4 @@
+export { FormAnalista } from "./form-analista";
+export { FormAta } from "./form-ata";
+export { FormCondutor } from "./form-condutor";
+export { FormPesquisador } from "./form-pesquisador";
