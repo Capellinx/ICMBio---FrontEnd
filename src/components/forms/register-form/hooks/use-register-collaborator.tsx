997c86c735ca7fa666bee2b723d20958a6f6a4ae
capellinx <capellaaa7@gmail.com>
2024-10-09 21:49:59 -0300

    feat: add onMutate in hook register

--- src/components/forms/register-form/hooks/use-register-collaborator.tsx
+++ src/components/forms/register-form/hooks/use-register-collaborator.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import { registerSchema } from "@/schemas/register"
 import { api } from "@/services/api"
 import { PersonType } from "@/services/auth"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
 import { toast } from "react-toastify"
+import { z } from "zod"
 
 export type CreateCollaboratorDTO = {
    name: string
@@ -18,35 +22,39 @@ export type CreateCollaboratorDTO = {
 export function useRegisterCollaborator() {
    const route = useRouter()
 
+   const form = useForm<z.infer<typeof registerSchema>>({
+      resolver: zodResolver(registerSchema),
+      mode: 'onBlur',
+   });
+
    const { mutate: handleRegisterCollaborator } = useMutation({
       mutationKey: ['register-collaborator'],
       mutationFn: (payload: CreateCollaboratorDTO) => reigsterCollaborator(payload),
+      onMutate: () => {
+         toast.loading("Cadastrando colaborador...", {
+            toastId: "register-collaborator", 
+         });
+      },
       onSuccess: () => {
-         toast.success('Conta criada com sucesso!', {
-            position: 'top-center',
+         toast.update("register-collaborator", {
+            render: 'Colaborador cadastrado com sucesso!',
+            type: "success",
+            isLoading: false, 
             autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-         })
-
-         // route.replace('/login')
+         });
+         route.replace('/login');
       },
       onError: () => {
-         toast.error('Falha ao criar a conta!', {
-            position: 'top-center',
+         toast.update("register-collaborator", {
+            render: 'Erro ao cadastrar colaborador!',
+            type: "error",
+            isLoading: false,
             autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-         })
+         });
+         form.setError("email", { message: "E-mail já cadastrado" });
       }
    })
-
+   
    async function reigsterCollaborator({ name, email, person_type, phone, cpf, matricula }: CreateCollaboratorDTO) {
       console.log(matricula);
       const { data } = await api.post("/collaborator", {
@@ -61,5 +69,5 @@ export function useRegisterCollaborator() {
       return data
    }
 
-   return { handleRegisterCollaborator }
+   return { handleRegisterCollaborator, form }
 }
